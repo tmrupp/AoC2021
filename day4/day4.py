7821eb6d1423e0b8e65fcf563c1d53c2031ffef6
tmrupp <tmrupp@umich.edu>
2021-12-06 09:20:22 -0700

    works

--- day4/day4.py
+++ day4/day4.py
@@ -3,72 +3,73 @@ import operator
 import itertools
 import re
 
-# class spot:
-#     def __init__ (self, value: int, selected=False):
-#         self.value = value
-#         self.selected = selected
-
-#     def __str__ (self):
-#         return str(self.value) + ", " + str(self.selected)
-
-# class board:
-#     def __init__ (self, values):
-#         self.board = 
-
-#     def __str__(self) -> str:
-#         s = ""
-#         for xs in self.board:
-#             for x in xs:
-#                 s = s + "(" + str(x) + ") "
-#             s = s + "\n"
-#         return s 
-
 def checkWin (bs):
     for x in range(0, len(bs[0])):
         if all([y[1] for y in bs[x]]) or all([y[1] for y in list(map(operator.itemgetter(x), bs))]):
             return True
     return False
 
-def score (b):
+def score (xss):
     s = 0
-    for xs in b.board:
-        for x in xs:
-            if not x.selected:
-                s += x.value
-    return s
-
-def mark (n, bs):
-    xss = bs
     for xs in xss:
         for x in xs:
-            if x[0] == n:
-                x = (x[0], True)
+            if not x[1]:
+                s += x[0]
+    return s
 
-    return xss
+def mark (n, xss):
+    yss = []
+    for xs in xss:
+        ys = []
+        for x in xs:
+            if x[0] == n:
+                ys.append((x[0], True))
+            else:
+                ys.append(x)
+        yss.append(ys)
+    return yss
     
 def findWinner (numbers, boards):
     n = numbers[0]
-    print (n)
-    bs = list(map(functools.partial(mark, n), boards))  
+    # print (n)
+    bs = list(map(functools.partial(mark, n), boards))
+    
 
     for b in bs:
         if checkWin(b):
-            print(score(b), n, b)
+            # print(score(b), n, b)
             return score(b)*n
-        print(b)
-
-    if len(numbers) == 1:
-        return False
+        # print(b)
     
     return findWinner(numbers[1:], bs)
 
+def findLoser (numbers, boards):
+    n = numbers[0]
+    # print (n)
+    bs = list(map(functools.partial(mark, n), boards))
+
+    xss = []
+    for b in bs:
+        if checkWin(b):
+            # print(score(b), n, b)
+            if len(bs) == 1:
+                return score(b)*n
+        else:
+            xss.append(b)
+        # print(b)
+    
+    return findLoser(numbers[1:], xss)
+
 
 if __name__ == "__main__":
-    f = open("test4.in", "r")
+    f = open("day4.in", "r")
     # f = open("test3.in", "r")
     data = f.readlines()
     numbers = list(map(int, data[0].split(','))) # not a list
     boards = [[[(int(x), False) for x in re.split(r'[ \n]', s) if x.isnumeric()] for s in data[x:x+5]] for x in range(2, len(data), 6)]
 
+    # part 1
     print(findWinner(numbers, boards))
+    # part 2
+    print(findLoser(numbers, boards))
 
