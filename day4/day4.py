373e0c0d9dda4083d424d35ef514d8777fecc46a
tmrupp <tmrupp@umich.edu>
2021-12-05 19:09:32 -0700

    more work on day4, dont want to make an iterable class

--- day4/day4.py
+++ day4/day4.py
@@ -3,42 +3,72 @@ import operator
 import itertools
 import re
 
-class spot:
-    def __init__ (self, value: int, selected=False):
-        self.value = value
-        self.selected = selected
+# class spot:
+#     def __init__ (self, value: int, selected=False):
+#         self.value = value
+#         self.selected = selected
 
-    def __str__ (self):
-        return str(self.value) + ", " + str(self.selected)
+#     def __str__ (self):
+#         return str(self.value) + ", " + str(self.selected)
 
-class board:
-    def __init__ (self, values):
-        # for s in values:
-        #     print (s.split(' '))
+# class board:
+#     def __init__ (self, values):
+#         self.board = 
 
-        self.board = [[spot(int(x)) for x in re.split(r'[ \n]', s) if x.isnumeric()] for s in values]
+#     def __str__(self) -> str:
+#         s = ""
+#         for xs in self.board:
+#             for x in xs:
+#                 s = s + "(" + str(x) + ") "
+#             s = s + "\n"
+#         return s 
 
-    def __str__(self) -> str:
-        s = ""
-        for xs in self.board:
-            for x in xs:
-                s = s + "(" + str(x) + ") "
-            s = s + "\n"
-        return s 
+def checkWin (bs):
+    for x in range(0, len(bs[0])):
+        if all([y[1] for y in bs[x]]) or all([y[1] for y in list(map(operator.itemgetter(x), bs))]):
+            return True
+    return False
 
-def checkWin (b):
+def score (b):
+    s = 0
+    for xs in b.board:
+        for x in xs:
+            if not x.selected:
+                s += x.value
+    return s
+
+def mark (n, bs):
+    xss = bs
+    for xs in xss:
+        for x in xs:
+            if x[0] == n:
+                x = (x[0], True)
+
+    return xss
     
+def findWinner (numbers, boards):
+    n = numbers[0]
+    print (n)
+    bs = list(map(functools.partial(mark, n), boards))  
 
-# def generateBoards (xs):
+    for b in bs:
+        if checkWin(b):
+            print(score(b), n, b)
+            return score(b)*n
+        print(b)
+
+    if len(numbers) == 1:
+        return False
     
+    return findWinner(numbers[1:], bs)
+
 
 if __name__ == "__main__":
     f = open("test4.in", "r")
     # f = open("test3.in", "r")
     data = f.readlines()
-    numbers = map(int, data[0].split(',')) # not a list
-    boards = [board(data[x:x+5]) for x in range(2, len(data), 6)]
+    numbers = list(map(int, data[0].split(','))) # not a list
+    boards = [[[(int(x), False) for x in re.split(r'[ \n]', s) if x.isnumeric()] for s in data[x:x+5]] for x in range(2, len(data), 6)]
 
-    # for b in boards:
-    #     print (b)
+    print(findWinner(numbers, boards))
 
