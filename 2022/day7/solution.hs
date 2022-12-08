4bba424dc6a94dd0696f0a45da76e0529bcd83d3
tmrupp <tmrupp@umich.edu>
2022-12-07 17:39:40 -0700

    no resolution in sight

--- 2022/day7/solution.hs
+++ 2022/day7/solution.hs
@@ -8,7 +8,7 @@ import Data.Maybe
 import Debug.Trace
 
 data FileSystem = FileSystem {
-    dirs :: Map String ([String], Int),
+    dirs :: Map String ([String], Integer),
     hist :: [String],
     cmds :: [[String]]
 } deriving (Show)
@@ -42,19 +42,19 @@ cd fs dir
     | dir == "/" = up fs
     | otherwise = into fs dir
 
-defaultEntry :: Maybe ([a], Int) -> ([a], Int)
+defaultEntry :: Maybe ([a], Integer) -> ([a], Integer)
 defaultEntry = fromMaybe ([], 0)
 
-addToSize :: Maybe ([String], Int) -> Int -> ([String], Int)
+addToSize :: Maybe ([String], Integer) -> Integer -> ([String], Integer)
 addToSize xs x  = (fst (defaultEntry xs), snd (defaultEntry xs) + x)
 
--- addToDir :: String -> Int -> ([String], Int)
--- addToDir dir size = 
+addFileToAll :: FileSystem -> Integer -> FileSystem
+addFileToAll fs size = trace (Prelude.show (hist fs) ++ " size: " ++ Prelude.show size) foldl (addFile size) fs (hist fs) 
 
-addFile :: FileSystem -> Int -> FileSystem
-addFile fs size = fs {dirs=Data.Map.insert (top fs) (addToSize (Data.Map.lookup (top fs) (dirs fs)) size) (dirs fs)}
+addFile :: Integer -> FileSystem -> String -> FileSystem
+addFile size fs dir = fs {dirs=Data.Map.insert dir (addToSize (Data.Map.lookup dir (dirs fs)) size) (dirs fs)}
 
-addToDirs :: Maybe ([String], Int) -> String -> ([String], Int)
+addToDirs :: Maybe ([String], Integer) -> String -> ([String], Integer)
 addToDirs xs x  = (x : fst (defaultEntry xs), snd (defaultEntry xs))
 
 addDir :: FileSystem -> String -> FileSystem
@@ -63,13 +63,13 @@ addDir fs dir = fs {dirs=Data.Map.insert (top fs) (addToDirs (Data.Map.lookup (t
 ls fs
     | null $ cmds fs = fs
     | head (cmd fs) == "$" || null (cmds fs) = fs
-    | all isDigit (head (cmd fs)) = ls $ addFile (exe fs) (read $ getSize fs)
+    | all isDigit (head (cmd fs)) = ls $ addFileToAll (exe fs) (read $ getSize fs) 
     | otherwise = ls $ addDir (exe fs) (getCmd fs)
 
 
-outputDirs :: [(String, ([String], Int))] -> String
+outputDirs :: [(String, ([String], Integer))] -> String
 outputDirs [] = ""
-outputDirs (dir:dirs) = (fst dir) ++ ", "  ++ (concat (fst (snd dir))) ++ " : " ++ (Prelude.show $ snd (snd dir)) ++ (outputDirs dirs)
+outputDirs (dir:dirs) = (fst dir) ++ ", "  ++ (foldl (\a b -> a ++ " " ++ b) "" (fst (snd dir))) ++ " : " ++ (Prelude.show $ snd (snd dir)) ++ "\n" ++ (outputDirs dirs)
 
 output fs = Prelude.show (hist fs) ++ " cmds=" ++ Prelude.show (cmds fs) ++ " size of dict=" ++ (Prelude.show $ Data.Map.size (dirs fs)) ++ " dict=" ++ Prelude.show (outputDirs $ Data.Map.toList (dirs fs))
 
@@ -92,6 +92,8 @@ part1 fs = sum $ filter (< 100000) (map (snd . snd) (Data.Map.toList (dirs fs)))
 
 part2 xs = xs
 
+-- 1036344 too low
+
 main = do
     handle <- openFile "day.in" ReadMode
     contents <- hGetContents handle
@@ -100,6 +102,8 @@ main = do
     -- print $ Data.Map.toList $ dirs FileSystem { dirs=Data.Map.fromList [("A", (["a", "b", "c"],1)),("B", (["a", "b", "c"],3)),("C", (["a", "b", "c"],2))] }
     let fs = execute $ newFileSystem (map words $ lines contents)
     print "part 1"
+    putStrLn $ outputDirs (Data.Map.toList (dirs fs))
+    print $ filter (< 100000) (map (snd . snd) (Data.Map.toList (dirs fs)))
     print $ part1 fs
     -- print $ part2 contents
     hClose handle   
