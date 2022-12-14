7c81ca6e88ce87d11178b2cc852d8d717ff559c8
tmrupp <tmrupp@umich.edu>
2022-12-14 09:54:04 -0700

    part1 and 2

--- 2022/day11/solution.hs
+++ 2022/day11/solution.hs
@@ -8,6 +8,7 @@ data Monkey = Monkey {
   items :: [Int], -- my items
   op :: Int -> Int, -- what op do I do?
   check :: Int -> Int, -- check which monkey to throw to
+  checkNum :: Int,
   count :: Int
 }
 
@@ -17,8 +18,11 @@ instance Show Monkey where
 parseItems :: String -> [Int]
 parseItems xs = map read $ drop 2 $ words $ [x | x <- xs, x /= ',']
 
+-- parseOp1 :: String -> (Int -> Int)
+-- parseOp1 xs x = parseOp xs x `div` 3
+
 parseOp :: String -> (Int -> Int)
-parseOp xs = \x -> op x (if xss !! 5 == "old" then x else read (xss !! 5)) `div` 3
+parseOp xs = \x -> op x (if xss !! 5 == "old" then x else read (xss !! 5))
   where (xss, op) = (words xs, if xss !! 4 == "+" then (+) else (*))
 
 getFrom xs line word = read $ words (xs !! line) !! word
@@ -30,7 +34,7 @@ parseCheck xs x
     else
         getFrom xs 2 5
 
-parseMonkey xs = trace (show $ length $ drop 3 xs) Monkey {count = 0, items = parseItems (xs !! 1), op = parseOp (xs !! 2), check = parseCheck (drop 3 xs)}
+parseMonkey xs = trace (show $ length $ drop 3 xs) Monkey {count = 0, items = parseItems (xs !! 1), op = parseOp (xs !! 2), check = parseCheck (drop 3 xs), checkNum=getFrom xs 3 3}
 
 makeMonkeys :: [String] -> [Monkey]
 makeMonkeys xs
@@ -65,6 +69,7 @@ monkeyRound ms n
   | otherwise = monkeyRound (executeMonkey ms n) n
 
 -- monkeyRounds ms n = monkeyRounds ms 0 n
+monkeyRounds :: (Eq t, Num t) => [Monkey] -> t -> [Monkey]
 monkeyRounds ms n
   | n == 0 = ms
   | otherwise = monkeyRounds (monkeyRound ms 0) (n - 1)
@@ -72,16 +77,35 @@ monkeyRounds ms n
 getHighest :: [Monkey] -> Int -> [Monkey]
 getHighest ms n = take n $ sortBy (\a b -> compare (count b) (count a)) ms
 
-part1 monkeys = product $ map (count) $ getHighest (monkeyRounds monkeys 20) 2
+getCommon :: [Monkey] -> Int
+getCommon xs = product (map checkNum xs)
+
+modOp :: Int -> Monkey -> Monkey
+modOp n m = m {op=(\x -> op m x `mod` n)}
+
+divOp :: Monkey -> Monkey
+divOp m = m {op=(\x -> op m x `div` 3)}
+
+updateMonkeyOps :: [Monkey] -> [Monkey]
+updateMonkeyOps ms = map (modOp common) ms
+  where common = getCommon ms
+
+div3Ops :: [Monkey] -> [Monkey]
+div3Ops ms = map (divOp) ms
+
+part1 monkeys = product $ map (count) $ getHighest (monkeyRounds (div3Ops monkeys) 20) 2
+part2 monkeys = product $ map (count) $ getHighest (monkeyRounds (updateMonkeyOps monkeys) 10000) 2
 
 main = do
     handle <- openFile "day.in" ReadMode
     contents <- hGetContents handle
     -- print $ parseItems " Starting items: 54, 65, 75, 74"
     let monkeys = makeMonkeys (lines contents)
+    let p2monkeys = updateMonkeyOps monkeys
     print $ monkeys
     print $ monkeyRound monkeys 0
-    print $ monkeyRounds monkeys 20
+    -- print $ monkeyRounds monkeys 10000
     print $ part1 monkeys
+    print $ part2 monkeys
 
     hClose handle   
